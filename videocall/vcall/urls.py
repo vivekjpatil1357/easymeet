101eba8d968bfcd2641504a1a15d8df48df23d50
vivek patil <vivekjpatil1357@gmail.com>
2023-07-18 16:37:11 +0530

    a

--- videocall/vcall/urls.py
+++ videocall/vcall/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views  import *
 urlpatterns = [
     path('',index,name="index"),
-    path('/login/',login,name='login'),
+    path('login/',login,name='login'),
     path('home/<username>',home,name='home'),
     path('generate/<username>',generate,name='generate'),
     path('meet/<str:username>/<str:link>',meet,name='meet'),
